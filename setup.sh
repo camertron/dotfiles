32256c1276399ea76e38689eaed7dc8b9985ffed
Cameron Dutro <camertron@gmail.com>
2021-09-20 15:58:06 -0700

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git clone git@github.com:camertron/dotfiles.git ~/dotfiles
+
+cat <<'EOF' >> ~/.bashrc
+
+for file in ~/.bash_profile_includes/*.sh; do
+  [[ -r $file ]] && source $file;
+done
+EOF
+
+cp -r ~/dotfiles/.bash_profile_includes/ ~/
+cp ~/dotfiles/.gitconfig ~/
+
+source ~/.bashrc
+
